86cfeef65e1e5b1f443cf440295e5b1cd6e927d4
Paxon Qiao 乔鹏军 <qiaopengjun0@gmail.com>
2024-11-15 00:06:13 +0800

    feature: add csv cli support (#4)

--- src/opts.rs
+++ src/opts.rs
@@ -1,14 +1,13 @@
 use clap::Parser;
 
 #[derive(Debug, Parser)]
-#[command(name="rcli", version, author, about, long_about=None)]
+#[command(name = "rcli", version, author, about, long_about = None)]
 pub struct Opts {
     #[command(subcommand)]
     pub cmd: SubCommand,
 }
 
 #[derive(Debug, Parser)]
-
 pub enum SubCommand {
     #[command(name = "csv", about = "Convert CSV to JSON")]
     Csv(CsvOpts),
@@ -19,13 +18,13 @@ pub struct CsvOpts {
     #[arg(short, long, value_parser = verify_input_file)]
     pub input: String,
 
-    #[arg(short, long, default_value = "output.json")]
+    #[arg(short, long, default_value = "output.json")] // "output.json".into()
     pub output: String,
 
     #[arg(short, long, default_value_t = ',')]
     pub delimiter: char,
 
-    #[arg(long, default_value_t = true)]
+    #[arg(long, default_value_t = true)] // short = 'h'
     pub header: bool,
 }
 
