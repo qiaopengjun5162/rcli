dec05135ab98383d2c92b468501f2d1e3ae8b45a
QiaoPengjun5162 <qiaopengjun0@gmail.com>
2024-11-15 23:09:08 +0800

    feature: support zxcvbn to test password strength

--- src/process/gen_pass.rs
+++ src/process/gen_pass.rs
@@ -1,4 +1,5 @@
 use rand::seq::SliceRandom;
+use zxcvbn::zxcvbn;
 
 const UPPER: &[u8] = b"ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 const LOWER: &[u8] = b"abcdefghijklmnopqrstuvwxyz";
@@ -41,8 +42,13 @@ pub fn process_genpass(
     }
     password.shuffle(&mut rng);
 
-    // TODO: make sure the password has at least one character of each type
+    let password = String::from_utf8(password)?;
 
-    println!("{}", String::from_utf8(password)?);
+    println!("{}", password);
+
+    // output password strength in stderr
+    let estimate = zxcvbn(&password, &[]);
+    eprintln!("Password strength: {}", estimate.score());
+
     Ok(())
 }
