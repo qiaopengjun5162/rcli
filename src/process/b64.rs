7f23cb60710a567bce34d00c3cdc812ec2e6ce8d
Paxon Qiao 乔鹏军 <qiaopengjun0@gmail.com>
2024-11-17 17:22:39 +0800

    feature: support base64 encode/decode cli (#8)

---
+++ src/process/b64.rs
@@ -0,0 +1,73 @@
+use std::{fs::File, io::Read};
+
+use crate::Base64Format;
+use anyhow::Result;
+use base64::{
+    engine::general_purpose::{STANDARD, URL_SAFE_NO_PAD},
+    Engine as _,
+};
+
+pub fn process_encode(input: &str, format: Base64Format) -> Result<()> {
+    let mut reader = get_reader(input)?;
+    let mut buf = Vec::new();
+    reader.read_to_end(&mut buf)?;
+
+    let encoded = match format {
+        Base64Format::Standard => STANDARD.encode(&buf),
+        Base64Format::UrlSafe => URL_SAFE_NO_PAD.encode(&buf),
+    };
+    println!("{}", encoded);
+    Ok(())
+}
+
+pub fn process_decode(input: &str, format: Base64Format) -> Result<()> {
+    let mut reader: Box<dyn Read> = get_reader(input)?;
+    let mut buf = String::new();
+    reader.read_to_string(&mut buf)?;
+    // avoid accidental newlines
+    let buf = buf.trim();
+    let decoded = match format {
+        Base64Format::Standard => STANDARD.decode(buf)?,
+        Base64Format::UrlSafe => URL_SAFE_NO_PAD.decode(buf)?,
+    };
+
+    // TODO: decoded data might not be string (but for this example, we assume it is)
+    println!("{}", String::from_utf8(decoded)?);
+    Ok(())
+}
+
+fn get_reader(input: &str) -> Result<Box<dyn Read>> {
+    let reader: Box<dyn Read> = if input == "-" {
+        Box::new(std::io::stdin())
+    } else {
+        Box::new(File::open(input)?)
+    };
+
+    Ok(reader)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_encode() {
+        let input = "hello world";
+        let encoded = STANDARD.encode(input.as_bytes());
+        assert_eq!(encoded, "aGVsbG8gd29ybGQ=");
+    }
+
+    #[test]
+    fn test_decode() {
+        let input = "aGVsbG8gd29ybGQ=";
+        let decoded = STANDARD.decode(input.as_bytes()).unwrap();
+        assert_eq!(String::from_utf8(decoded).unwrap(), "hello world");
+    }
+
+    #[test]
+    fn test_process_decode() {
+        let input = "fixtures/b64.txt";
+        let format = Base64Format::UrlSafe;
+        assert!(process_decode(input, format).is_ok());
+    }
+}
