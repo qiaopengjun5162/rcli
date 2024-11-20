c14b638cb45528c99badb60e75f85d2bdb10e9b7
Paxon Qiao 乔鹏军 <qiaopengjun0@gmail.com>
2024-11-20 22:09:43 +0800

    feature: add sign/verify/generate cli for text (#9)

--- README.md
+++ README.md
@@ -1,3 +1,21 @@
 # RCLI
 
 rcli is a rust cli tool.
+
+## Usage
+
+```sh
+rcli on  feature/text [!?] is 📦 0.1.0 via 🦀 1.82.0 via 🅒 base
+➜ cargo run -- text sign -k fixtures/ed25519.sk --format ed25519
+    Finished `dev` profile [unoptimized + debuginfo] target(s) in 0.26s
+     Running `target/debug/rcli text sign -k fixtures/ed25519.sk --format ed25519`
+hello!^D
+KiS91TPswopDjf7v6oRjA9-U57SMuG9VYcpQ9XWh8vYsACFwLJ6anUG-Nw3ZCqJN_-Bdd67op5xd1cQ8pRayBQ
+
+rcli on  feature/text [!?] is 📦 0.1.0 via 🦀 1.82.0 via 🅒 base took 7.9s
+➜ cargo run -- text verify -k fixtures/ed25519.pk --format ed25519 --sig KiS91TPswopDjf7v6oRjA9-U57SMuG9VYcpQ9XWh8vYsACFwLJ6anUG-Nw3ZCqJN_-Bdd67op5xd1cQ8pRayBQ
+    Finished `dev` profile [unoptimized + debuginfo] target(s) in 0.23s
+     Running `target/debug/rcli text verify -k fixtures/ed25519.pk --format ed25519 --sig KiS91TPswopDjf7v6oRjA9-U57SMuG9VYcpQ9XWh8vYsACFwLJ6anUG-Nw3ZCqJN_-Bdd67op5xd1cQ8pRayBQ`
+hello!
+true
+```
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,13 @@
+use std::fs;
+
 use anyhow::Ok;
 use clap::Parser;
 use rcli::{
-    process_csv, process_decode, process_encode, process_genpass, Base64SubCommand, Opts,
-    SubCommand,
+    process_csv, process_decode, process_encode, process_genpass, process_text_generate,
+    process_text_sign, process_text_verify, Base64SubCommand, Opts, SubCommand, TextSignFormat,
+    TextSubCommand,
 };
+use zxcvbn::zxcvbn;
 
 fn main() -> anyhow::Result<()> {
     let opts = Opts::parse();
@@ -17,20 +21,53 @@ fn main() -> anyhow::Result<()> {
             process_csv(&opts.input, output, opts.format)?
         }
         SubCommand::GenPass(opts) => {
-            process_genpass(
+            let password = process_genpass(
                 opts.length,
                 opts.uppercase,
                 opts.lowercase,
                 opts.numbers,
                 opts.symbols,
             )?;
+            println!("{}", password);
+
+            // output password strength in stderr
+            let estimate = zxcvbn(&password, &[]);
+            eprintln!("Password strength: {}", estimate.score());
         }
         SubCommand::Base64(subcmd) => match subcmd {
             Base64SubCommand::Encode(opts) => {
-                process_encode(&opts.input, opts.format)?;
+                let encoded = process_encode(&opts.input, opts.format)?;
+                println!("{}", encoded);
             }
             Base64SubCommand::Decode(opts) => {
-                process_decode(&opts.input, opts.format)?;
+                let decoded = process_decode(&opts.input, opts.format)?;
+                // TODO: decoded data might not be string (but for this example, we assume it is)
+                println!("{}", String::from_utf8(decoded)?);
+            }
+        },
+        SubCommand::Text(subcmd) => match subcmd {
+            TextSubCommand::Sign(opts) => {
+                let signed = process_text_sign(&opts.input, &opts.key, opts.format)?;
+                println!("{}", signed);
+            }
+            TextSubCommand::Verify(opts) => {
+                let verified = process_text_verify(&opts.input, &opts.key, opts.format, &opts.sig)?;
+                println!("{}", verified);
+            }
+            TextSubCommand::Generate(opts) => {
+                let key = process_text_generate(opts.format)?;
+                match opts.format {
+                    TextSignFormat::Blake3 => {
+                        let name = opts.output.join("blake3.txt");
+                        fs::write(name, &key[0])?;
+                    }
+                    TextSignFormat::Ed25519 => {
+                        let name = &opts.output;
+
+                        fs::write(name.join("ed25519.sk"), &key[0])?;
+                        fs::write(name.join("ed25519.pk"), &key[1])?;
+                    }
+                }
             }
         },
     }
