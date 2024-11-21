3ae310bac5ca9545459e5145f6df0ff3290cf906
QiaoPengjun5162 <qiaopengjun0@gmail.com>
2024-11-21 23:33:58 +0800

    feature: add tower-http support

--- src/process/http_serve.rs
+++ src/process/http_serve.rs
@@ -7,7 +7,7 @@ use axum::{
     routing::get,
     Router,
 };
-
+use tower_http::services::ServeDir;
 use tracing::{info, warn};
 
 #[derive(Debug)]
@@ -18,14 +18,21 @@ struct HttpServeState {
 pub async fn process_http_serve(path: PathBuf, port: u16) -> Result<()> {
     let addr = SocketAddr::from(([0, 0, 0, 0], port));
     info!("Serving {:?} on  {}", path, addr);
-    let state = HttpServeState { path };
+    let state = HttpServeState { path: path.clone() };
 
-    let app = Router::new()
+    let dir_service = ServeDir::new(path)
+        .append_index_html_on_directories(true)
+        .precompressed_br()
+        .precompressed_deflate()
+        .precompressed_gzip()
+        .precompressed_zstd();
+    let router = Router::new()
         .route("/*path", get(file_handler))
+        .nest_service("/tower", dir_service)
         .with_state(Arc::new(state));
 
     let listener = tokio::net::TcpListener::bind(addr).await?;
-    axum::serve(listener, app).await?;
+    axum::serve(listener, router).await?;
     Ok(())
 }
 
@@ -41,6 +48,10 @@ async fn file_handler(
             format!("File {:?} not found", p.display()),
         )
     } else {
+        // TODO: check if file is a directory
+        // if it's a directory, list all files/subdirectories
+        // as <li><a href="path/to/file">file name</a></li>
+        // <html><body><ul>...</ul></body></html>
         match tokio::fs::read_to_string(&p).await {
             Ok(content) => {
                 info!("Read {} bytes", content.len());
@@ -54,5 +65,22 @@ async fn file_handler(
                 )
             }
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[tokio::test]
+    async fn test_file_handler() {
+        let state = Arc::new(HttpServeState {
+            path: PathBuf::from("."),
+        });
+        let (status, content) = file_handler(State(state), Path("Cargo.toml".to_string())).await;
+        assert_eq!(status, StatusCode::OK);
+        assert!(!content.is_empty());
+        assert!(content.contains("version"));
+        assert!(content.trim().starts_with("[package]"));
     }
 }
